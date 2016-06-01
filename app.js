aab11feccb80d2bcb0cf3f7304e230dc4a0692ce
Bryan Yee <yee.bryan.c@gmail.com>
2016-05-31 22:23:05 -0700

    Show a results box when the countdown reaches zero

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var maxXAxis = window.innerWidth - 124, maxYAxis = window.innerHeight - 44;
 var locationX = maxXAxis/2, locationY = maxYAxis/2;
 var buttonCounter = 0, timerValue = 10;
 var timer;
+var resultsBox = document.getElementById("resultsBox");
 
 //set the button's initial location
 button.style.left = locationX.toString() + "px";
@@ -12,6 +13,7 @@ button.style.top = locationY.toString() + "px";
 button.onmouseenter = function(){
 	//Sets the timer and button counter on the first mouseenter
 	if (timer === undefined){
+		resultsBox.style.display = "none";
 		timerValue = 10;
 		rightHeader.innerHTML = timerValue;
 		rightHeader.style.color = "red";
@@ -46,7 +48,13 @@ function countdown(){
 		clearInterval(timer);
 		timer = undefined;
 		rightHeader.style.color = "white";
+		
+		showResults();
 	}
+}
+
+function showResults(){
+	resultsBox.style.display = "block";
 }
 
 
