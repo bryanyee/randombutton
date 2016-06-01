de4e2f75b9f8e267055fd16d3ea125b8ade7f597
Bryan Yee <yee.bryan.c@gmail.com>
2016-05-31 21:46:04 -0700

    Abbreviated alternate function (in comments)

--- app.js
+++ app.js
@@ -52,28 +52,6 @@ function countdown(){
 
 /*Alternate onmouse event function
 button.addEventListener("mouseenter", function(){
-	//Sets the timer
-	if (timer === undefined){
-		timerValue = 10;
-		rightHeader.innerHTML = timerValue;
-		rightHeader.style.color = "red";
-		timer = setInterval(countdown, 1000);
-	}
-
-	//Sets a counter in the button
-	buttonCounter++;
-	button.innerHTML = "<p>" + buttonCounter + "</p>";
-
-	//window height and width are calculated within the function to adjust if the user changes the window size
-	maxXAxis = window.innerWidth - 124;
-	maxYAxis = window.innerHeight - 44;
-
-	//calculates a random location for the button, based on the current window size
-	locationX = Math.random() * maxXAxis;
-	locationY = Math.random() * maxYAxis;
-
-	//sets the location properties of the button
-	button.style.left = locationX.toString() + "px";
-	button.style.top = locationY.toString() + "px";
+	//same code as the button.onmouseenter function
 });
 */
