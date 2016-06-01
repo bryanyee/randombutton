13e7dcc9ce9a37c0a9d935314441e40b73e333e3
Bryan Yee <yee.bryan.c@gmail.com>
2016-05-31 19:21:07 -0700

    First commit - random buttom

---
+++ app.js
@@ -0,0 +1,34 @@
+var button = document.getElementById("button");
+var maxXAxis, maxYAxis, locationX, locationY;
+
+button.onmouseenter = function(){
+	//window height and width are calculated within the function to adjust if the user changes the window size
+	maxXAxis = window.innerWidth - 124;
+	maxYAxis = window.innerHeight - 44;
+
+	//calculates a random location for the button, based on the current window size
+	locationX = Math.random() * maxXAxis;
+	locationY = Math.random() * maxYAxis;
+
+	//sets the location properties of the button
+	button.style.left = locationX.toString() + "px";
+	button.style.top = locationY.toString() + "px";
+};
+
+
+
+/*Alternate function
+button.addEventListener("mouseenter", function(){
+	maxXAxis = window.innerWidth - 124;
+	maxYAxis = window.innerHeight - 44;
+
+	locationX = Math.random() * maxXAxis;
+	locationY = Math.random() * maxYAxis;
+
+	button.style.left = locationX.toString() + "px";
+	button.style.top = locationY.toString() + "px";
+});
+*/
+
+
+
